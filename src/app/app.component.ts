1b7a7253e9710d02946c7bfa0c2b5f4ad3d543b8
Oce14 <oceane.le-quellec@etu.univ-grenoble-alpes.fr>
2024-05-02 14:30:49 +0200

    feat:Adding libraries

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { HomeComponent } from "./components/home/home.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [
+        RouterOutlet,
+        MatSlideToggleModule,
+        HomeComponent,
+    ]
 })
 export class AppComponent {
   title = 'FrontEnd';
